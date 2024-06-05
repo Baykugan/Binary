5a2903a4f7f837323ae2b1d48f206dfb7f07a5b1
Bayukgan <Even.Ytterli.Tokle@gmail.com>
2024-06-05 16:18:50 +0200

    New: Initial commit of project

---
+++ binary.py
@@ -0,0 +1,13 @@
+"""
+Contains the Binary class.
+"""
+
+from typing import Any, Self
+
+
+class Binary:
+    """
+    Binary class
+
+    This class is used to represent a binary number.
+    """
---
+++ test.py
@@ -0,0 +1,18 @@
+"""
+Contains the unit tests for the Binary class.
+"""
+
+from binary import Binary
+import unittest
+
+
+class TestBinary(unittest.TestCase):
+    """
+    TestBinary class
+
+    This class contains the unit tests for the Binary class.
+    """
+
+
+if __name__ == "__main__":
+    unittest.main()
