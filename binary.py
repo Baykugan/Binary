94881fae57a4aa75b9276f15a2a2a47f9cf9420f
Baykugan <Even.Ytterli.Tokle@gmail.com>
2024-06-06 16:11:03 +0200

    Feat: Add functionalities for initializing class

--- binary.py
+++ binary.py
@@ -11,3 +11,121 @@ class Binary:
 
     This class is used to represent a binary number.
     """
+
+    #########################
+    ##### Magic methods #####
+    #########################
+
+    def __init__(self, value: Self | int | str | bytearray | list[int]) -> None:
+        self.value = Binary.from_value(value)
+
+    def __str__(self) -> str:
+        return "".join(
+            str(Binary._get_bit(self.value, i))
+            for i in reversed(range(len(self.value) * 8))
+        )
+
+    def __repr__(self) -> str:
+        return f"Binary({self.value})"
+
+    ##########################
+    ##### Public methods #####
+    ##########################
+
+    def to_decimal(self) -> int:
+        """
+        Convert the binary number to a decimal number.
+        """
+
+        return Binary._to_decimal(self)
+
+    #########################
+    ##### Class methods #####
+    #########################
+
+    @classmethod
+    def from_value(cls, value: Any) -> bytearray:
+        """
+        Checks the type of the value and calls the
+        appropriate method to create a byte array.
+        """
+
+        if isinstance(value, Binary):
+            return value.value
+        if isinstance(value, bytearray):
+            return value
+        if isinstance(value, int):
+            return cls.from_decimal(value)
+        if isinstance(value, str):
+            return cls.from_string(value)
+        if isinstance(value, list):
+            return cls.from_list(value)
+        raise ValueError("Invalid value")
+
+    @classmethod
+    def from_decimal(cls, decimal: int) -> bytearray:
+        """
+        Create a byte array from a decimal number.
+        """
+
+        binary = []
+        if decimal < 0:
+            raise ValueError("Invalid decimal value")
+        if decimal == 0:
+            binary.append(0)
+
+        while decimal > 0:
+            binary.append(decimal % 2)
+            decimal //= 2
+        return cls._create_bit_array(list(reversed(list(binary))))
+
+    @classmethod
+    def from_string(cls, string: str) -> bytearray:
+        """
+        Create a byte array from a binary string.
+        """
+
+        if not all(bit in "01" for bit in string):
+            raise ValueError("Invalid binary string")
+        return cls._create_bit_array([int(bit) for bit in string])
+
+    @classmethod
+    def from_list(cls, lst: list[int]) -> bytearray:
+        """
+        Create a byte array from a binary list.
+        """
+
+        if not all(bit in [0, 1] for bit in lst):
+            raise ValueError("Invalid binary list")
+        return cls._create_bit_array(lst)
+
+    ###########################
+    ##### Static methods ######
+    ###########################
+
+    @staticmethod
+    def _to_decimal(binary: Self) -> int:
+        decimal = 0
+        for i in range(len(binary.value) * 8):
+            decimal += Binary._get_bit(binary.value, i) * 2**i
+        return decimal
+
+    @staticmethod
+    def _create_bit_array(lst: list[int]) -> bytearray:
+        bit_array = bytearray((len(lst) + 7) // 8)
+        for position, bit in enumerate(reversed(lst)):
+            Binary._set_bit(bit_array, position, bit)
+        return bit_array
+
+    @staticmethod
+    def _get_bit(value: bytearray, position: int) -> int:
+        byte_index, bit_index = divmod(position, 8)
+        return (value[byte_index] & (1 << bit_index)) >> bit_index
+
+    @staticmethod
+    def _set_bit(value: bytearray, position: int, bit: int) -> None:
+        byte_index, bit_index = divmod(position, 8)
+        if bit == 1:
+            value[byte_index] |= 1 << bit_index
+        else:
+            value[byte_index] &= ~(1 << bit_index)
--- test.py
+++ test.py
@@ -2,8 +2,9 @@
 Contains the unit tests for the Binary class.
 """
 
-from binary import Binary
 import unittest
+
+from binary import Binary
 
 
 class TestBinary(unittest.TestCase):
@@ -12,6 +13,211 @@ class TestBinary(unittest.TestCase):
 
     This class contains the unit tests for the Binary class.
     """
+
+    # pylint: disable=protected-access
+    def test_create_byte_array(self):
+        """
+        Test the _create_bit_array method.
+        """
+
+        
+        bit_array = Binary._create_bit_array([0])
+        self.assertEqual(
+            bit_array,
+            bytearray([0]),
+            "Failed to create byte array from list [0]",
+        )
+
+        bit_array = Binary._create_bit_array([1])
+        self.assertEqual(
+            bit_array,
+            bytearray([1]),
+            "Failed to create byte array from list [1]",
+        )
+
+        large_bit_array = Binary._create_bit_array([1] * 16)
+        self.assertEqual(
+            large_bit_array,
+            bytearray([255, 255]),
+            "Failed to create byte array from [1] * 16",
+        )
+    # pylint: enable=protected-access
+
+    def test_from_decimal(self):
+        """
+        Test the from_decimal method.
+        """
+
+        bit_array = Binary.from_decimal(0)
+        self.assertEqual(
+            bit_array,
+            bytearray([0]),
+            "Failed to create byte array from decimal 0",
+        )
+
+        bit_array = Binary.from_decimal(1)
+        self.assertEqual(
+            bit_array,
+            bytearray([1]),
+            "Failed to create byte array from decimal 1",
+        )
+
+        bit_array = Binary.from_decimal(2**16 - 1)
+        self.assertEqual(
+            bit_array,
+            bytearray([255, 255]),
+            "Failed to create byte array from decimal 2**16 - 1",
+        )
+
+    def test_from_decimal_invalid(self):
+        """
+        Test the from_decimal method with invalid input.
+        """
+
+        with self.assertRaises(ValueError, msg="Invalid decimal value"):
+            Binary.from_decimal(-1)
+
+    def test_from_string(self):
+        """
+        Test the from_string method.
+        """
+
+        bit_array = Binary.from_string("0")
+        self.assertEqual(
+            bit_array,
+            bytearray([0]),
+            "Failed to create byte array from string '0'",
+        )
+
+        bit_array = Binary.from_string("1")
+        self.assertEqual(
+            bit_array,
+            bytearray([1]),
+            "Failed to create byte array from string '1'",
+        )
+
+        bit_array = Binary.from_string("1" * 16)
+        self.assertEqual(
+            bit_array,
+            bytearray([255, 255]),
+            "Failed to create byte array from string '1' * 16",
+        )
+
+    def test_from_string_invalid(self):
+        """
+        Test the from_string method with invalid input.
+        """
+
+        with self.assertRaises(ValueError, msg="Invalid binary string"):
+            Binary.from_string("2")
+
+    def test_from_list(self):
+        """
+        Test the from_list method.
+        """
+
+        bit_array = Binary.from_list([0])
+        self.assertEqual(
+            bit_array,
+            bytearray([0]),
+            "Failed to create byte array from list [0]",
+        )
+
+        bit_array = Binary.from_list([1])
+        self.assertEqual(
+            bit_array,
+            bytearray([1]),
+            "Failed to create byte array from list [1]",
+        )
+
+        bit_array = Binary.from_list([1] * 16)
+        self.assertEqual(
+            bit_array,
+            bytearray([255, 255]),
+            "Failed to create byte array from list [1] * 16",
+        )
+
+    def test_from_list_invalid(self):
+        """
+        Test the from_list method with invalid input.
+        """
+
+        with self.assertRaises(ValueError, msg="Invalid binary list"):
+            Binary.from_list([2])
+
+    def test_to_decimal(self):
+        """
+        Test the to_decimal method.
+        """
+
+        binary = Binary(0)
+        self.assertEqual(
+            binary.to_decimal(),
+            0,
+            "Failed to convert byte array [0] to decimal",
+        )
+
+        binary = Binary(1)
+        self.assertEqual(
+            binary.to_decimal(),
+            1,
+            "Failed to convert byte array [1] to decimal",
+        )
+
+        binary = Binary(2**16 - 1)
+        self.assertEqual(
+            binary.to_decimal(),
+            2**16 - 1,
+            "Failed to convert byte array [255, 255] to decimal",
+        )
+
+    def test_from_value(self):
+        """
+        Test the from_value method.
+        """
+
+        binary = Binary.from_value(Binary(0))
+        self.assertEqual(
+            binary,
+            bytearray([0]),
+            "Failed to convert Binary object to byte array",
+        )
+
+        binary = Binary.from_value(bytearray([0]))
+        self.assertEqual(
+            binary,
+            bytearray([0]),
+            "Failed to convert byte array to byte array",
+        )
+
+        binary = Binary.from_value(0)
+        self.assertEqual(
+            binary,
+            bytearray([0]),
+            "Failed to convert decimal to byte array",
+        )
+
+        binary = Binary.from_value("0")
+        self.assertEqual(
+            binary,
+            bytearray([0]),
+            "Failed to convert string to byte array",
+        )
+
+        binary = Binary.from_value([0])
+        self.assertEqual(
+            binary,
+            bytearray([0]),
+            "Failed to convert list to byte array",
+        )
+
+    def test_from_value_invalid(self):
+        """
+        Test the from_value method with invalid input.
+        """
+
+        with self.assertRaises(ValueError, msg="Invalid value"):
+            Binary.from_value(None)
 
 
 if __name__ == "__main__":
